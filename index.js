c9a6f0bff4a730d47edab640f62a681a57be822f
Matthew Healey <matthewhealey06@gmail.com>
2026-03-03 15:28:33 +0000

    implement remember me feature

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const rememberMe = document.getElementById('rememberMe')
 const submit = document.getElementById('submit')
 const errorMsg = document.getElementById('error-message')
 
+const savedUser = JSON.parse(localStorage.getItem('savedUser'))
+if(savedUser){
+    username.value = savedUser.username
+    password.value = savedUser.password
+}
 submit.addEventListener('click', (e) => {
     const usernameValue = username.value
     const passwordValue = password.value
@@ -12,6 +17,16 @@ submit.addEventListener('click', (e) => {
 
     if(usernameValue === storedUser.username && passwordValue === storedUser.password){
         window.location.href = 'welcome.html'
+        if(rememberMe.checked){
+            const savedUser = {
+                username: usernameValue,
+                password: passwordValue
+            }
+        localStorage.setItem('savedUser', JSON.stringify(savedUser))
+        }
+        else{
+            localStorage.removeItem('savedUser')
+        }
     }
     else{
         errorMsg.textContent = 'Incorrect Username Or Password'
