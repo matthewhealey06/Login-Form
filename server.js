46655236d2ae3545257040e4a473bfa3863bc6c6
Matthew Healey <matthewhealey06@gmail.com>
2026-04-01 13:44:22 +0100

    Connect frontend registration to backend API

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
+app.use(express.json())
+app.post('/api/register', (req, res) => {
+    console.log(req.body)
+    res.json({ message: 'Data received' })
+})
 
 app.listen(3000, () => {
     console.log('The Server is Running')
